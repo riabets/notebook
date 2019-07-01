ccb3c8a7f6d417605fc9ec778165c7ff208a7535
Marina Riabets <riabets.mv@gmail.com>
2019-07-01 11:39:37 +0300

    App

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,31 +4,50 @@ workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 // install new service worker when ok, then reload page.
-let storageNotesAout = this.notesReminder;
+const urlB64ToUint8Array = base64String => {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+  const rawData = atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+};
 
-console.log(storageNotesAout);
+const saveSubscription = async subscription => {
+  const SERVER_URL = "https://notebook-994c8.web.app/";
+  const response = await fetch(SERVER_URL, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(subscription)
+  });
+  return response.json();
+};
 
-self.addEventListener('sync', event => {
-    if (event.tag === 'send-chats') {
-      event.waitUntil(
-        getMessagesFromOutbox().then(messages => {
-          // Post the messages to the server
-          return fetch('/send', {
-            method: 'POST',
-            body: JSON.stringify(messages),
-            headers: { 'Content-Type': 'application/json' }
-          }).then(() => {
-            // Success! Remove them from the outbox
-            return removeMessagesFromOutbox(messages);
-          });
-        }).then(() => {
-          // Tell pages of your success so they can update UI
-          return clients.matchAll({ includeUncontrolled: true });
-        }).then(clients => {
-          clients.forEach(client => client.postMessage('outbox-processed'))
-        })
-      );
-    }
+self.addEventListener("install", async () => {
+  // This will be called only once when the service worker is installed for first time.
+  try {
+    const applicationServerKey = urlB64ToUint8Array(
+      "BAyie1OFbDxscGJGaFb8sXbgw9GACXNXfY4shwROfR39NP92yIzDFX2tk02j8O0NYwqw1kuOJWrPmogyWx36MJk"
+    );
+    const options = { applicationServerKey, userVisibleOnly: true };
+    const subscription = await self.registration.pushManager.subscribe(options);
+    const response = await saveSubscription(subscription);
+    console.log(response);
+  } catch (err) {
+    console.log("Error", err);
+  }
 });
 
-
+self.addEventListener("push", function(event) {
+  if (event.data) {
+    console.log("Push event!! ", event.data.text());
+  } else {
+    console.log("Push event but no data");
+  }
+});
